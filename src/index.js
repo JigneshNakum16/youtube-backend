3e88effcd07b190080709ba12f34978ad797ee6f
JigneshNakum16 <jigneshnakum16@gmail.com>
2024-06-13 15:51:10 +0530

    connect to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import connectionDB from "./db/index.js";
+
+
+dotenv.config({
+    path: "./env"
+})
+
+connectionDB();
+
+// import express from "express";
+// const app = express();
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`);
+//     app.on((error) => {
+//       console.error("error", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`app is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("error", error);
+//     throw error;
+//   }
+// })();
